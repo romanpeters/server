60109290876be309bdd1e4a540d6e87072c47446
Roman Peters <mail@romanpeters.nl>
2025-04-17 21:59:55 +0200

    Revert Nix shell test script

--- tests/molecule/webserver/test.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env nix-shell
-#! nix-shell -i bash -p python3 python3Packages.molecule python3Packages.ansible docker
-set -euo pipefail
-
-# Run Molecule scenario for the webserver role
-molecule test -s webserver
